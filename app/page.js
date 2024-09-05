3f44649dd61a1fb23bf65483a352a82f8234fc07
treva556 <marktreva92@gmail.com>
2024-09-06 01:13:23 +0300

    milestone 4

--- app/page.js
+++ app/page.js
@@ -1,43 +1,55 @@
 
 // app/page.js
-import React from 'react';
+// app/page.js
+"use client"; // Add this directive to use client-side features
 
-export default async function Home() {
-  try {
-    const apiUrl = process.env.NEXT_PUBLIC_API_URL || '';
-    const res = await fetch(`${apiUrl}/api/posts`, {
-      cache: 'no-store',
-    });
-    if (!res.ok) {
-      throw new Error('Failed to fetch');
-    }
-    const posts = await res.json();
+import React, { useState, useEffect } from 'react';
 
-    console.log('Posts:', posts);  // Log the posts data
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL || '';
 
-    return (
-      <div>
-        <h1>Posts</h1>
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`${apiUrl}/api/posts`, {
+          cache: 'no-store',
+        });
+
+        if (!res.ok) {
+          throw new Error('Failed to fetch');
+        }
+
+        const data = await res.json();
+        setPosts(data);
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+        setError(error.message);
+      }
+    };
+
+    fetchPosts();
+  }, [apiUrl]);
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      {error ? (
+        <p>Error fetching posts: {error}</p>
+      ) : (
         <ul>
-          <div >
           {posts.length > 0 ? (
             posts.map(post => (
-              <li className=' p-4 border border-black ml-6 mr-8 border-b-zinc-50 ' key={post._id}>{post.name}</li>
+              <li className='p-4 border border-black ml-6 mr-8 border-b-zinc-50' key={post._id}>
+                {post.name}
+              </li>
             ))
           ) : (
             <li>No posts available</li>
           )}
-          </div>
         </ul>
-      </div>
-    );
-  } catch (error) {
-    console.error('Error fetching posts:', error);
-    return (
-      <div>
-        <h1>Posts</h1>
-        <p>Error fetching posts: {error.message}</p>
-      </div>
-    );
-  }
+      )}
+    </div>
+  );
 }
